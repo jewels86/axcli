8d6ba4f52e73932861f9c4319207ead2c4dc4013
jewels86 <jewels286@proton.me>
2025-01-07 08:26:45 -0500

    catalog stuff

--- axcli/catalog.py
+++ axcli/catalog.py
@@ -1,3 +1,17 @@
 import click
-import axinite.tools as axtools
-import url
+import httpx
+import json
+
+CATALOG = "https://raw.githubusercontent.com/jewels86/Axinite/refs/heads/main/templates/catalog.txt"
+
+@click.command("catalog")
+def catalog():
+    systems = {}
+    r_catalog = httpx.get(CATALOG)
+    content = r_catalog.text
+    for line in content.splitlines():
+        r = httpx.get(line)
+        system = json.loads(r.text)
+        systems[system["name"]] = system
+    
+    
