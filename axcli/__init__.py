b5a1a55be0e44dfee3243470b09bb5a013dfb3d8
jewels86 <jewels286@proton.me>
2025-01-08 09:18:50 -0500

    gotta fix some ax stuff

--- axcli/__init__.py
+++ axcli/__init__.py
@@ -2,3 +2,4 @@ from axcli.new import new
 from axcli.read import read
 from axcli.load import load
 from axcli.catalog import catalog
+from axcli.run import run
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ cli.add_command(new)
 cli.add_command(read)
 cli.add_command(load)
 cli.add_command(catalog)
+cli.add_command(run)
 
 if __name__ == "__main__":
     cli()
