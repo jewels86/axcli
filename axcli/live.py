604535c0ed2525654b0302200ca162356d1d5199
Jewels <jewels286@proton.me>
2025-01-08 15:29:16 -0500

    live stuff

--- axcli/live.py
+++ axcli/live.py
@@ -3,11 +3,10 @@ import click
 
 @click.command("live")
 @click.argument("path", type=click.Path(exists=True))
-@click.option("-f", "--frontend", type=str, default="plotly")
+@click.option("-f", "--frontend", type=str, default="vpython")
 def live(path, frontend):
     name_to_frontend = {
         "vpython": axtools.vpython_frontend,
-        "plotly": axtools.plotly_frontend,
     }
     args = axtools.read(path)
-    axtools.live(args, name_to_frontend[frontend](args, "show"))
+    axtools.live(args, name_to_frontend[frontend](args, "live"))
