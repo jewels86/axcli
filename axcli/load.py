5c860b2b9f628bdb2d23c3a1d74db64999c0f46a
Jewels <jewels286@proton.me>
2025-02-14 17:04:16 -0500

    1.2.15

--- axcli/load.py
+++ axcli/load.py
@@ -17,6 +17,7 @@ name_to_backend = {
 @click.option("-b", "--backend", type=str, default="verlet")
 def load(input_path, output_path, limit, delta, backend):
     "Load a system from a file."
+    print("Setting up simulation data...")
     args = axtools.read(input_path)
     args.backend = name_to_backend[backend]
     if delta != "-1": args.set_delta(ax.interpret_time(delta))
