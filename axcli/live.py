8104f7858174cf1abafd4b61c8d2678c698e4586
Jewels <jewels286@proton.me>
2025-02-23 18:50:36 -0500

    1.2.20

--- axcli/live.py
+++ axcli/live.py
@@ -5,11 +5,13 @@ import click
 @click.argument("path", type=click.Path(exists=True))
 @click.option("-f", "--frontend", type=str, default="vpython")
 @click.option("-s", type=int, default=1)
-def live(path, frontend, s):
+@click.option("-r", "--rate", type=int, default=-1)
+def live(path, frontend, s, r):
     "Watch a system live."
     name_to_frontend = {
         "vpython": axtools.vpython_frontend,
         "mpl": axtools.mpl_frontend,
     }
     args = axtools.read(path)
+    if r != -1: args["rate"] = r
     axtools.live(args, name_to_frontend[frontend](args, "live", s=s))
